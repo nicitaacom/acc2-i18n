65a68feef21931b595a03eaa0fd5a5658001594d
Nikita <nicitaacom@gmail.com>
2024-03-07 12:33:46 +0100

    go to client page

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -11,12 +11,15 @@ interface PageProps {
 export default async function Page({ params: { lng },path='' }:PageProps) {
    const { t } = await useTranslation(lng)
   return (
-    <>
+    <div className="flex flex-col">
       <h1>{t('title')}</h1>
       <Link href={`/${lng}/second-page`}>
         {t('to-second-page')}
       </Link>
+        <Link href={`/${lng}/client-page`}>
+        {t('to-client-page')}
+      </Link>
       <Footer lng={lng} />
-    </>
+    </div>
   )
 }
